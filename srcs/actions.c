4b4b5dcb95beea6d0fbd7bfb94febba37cb4aa06
nytsdayo <nyantsutsu@gmail.com>
2024-07-09 18:41:55 +0900

    PRESS ESC KEY

---
+++ srcs/actions.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:a+
+	+#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 22:25:30 by rnakatan          #+#    #+#             */
+/*   Updated: 2024/07/08 23:09:11 by rnakatan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+void	press_ESC(t_game *game)
+{
+	printf("Exit game\n");
+	mlx_destroy_window(game->mlx, game->win);
+	exit(0);
+}
+
+// void	move_forward(t_game *game)
+// {
+// 	printf("Move forward\n");
+// }
+
+// void	move_backward(t_game *game)
+// {
+// 	printf("Move backward\n");
+// }
+
+// void	move_left(t_game *game)
+// {
+// 	printf("Move left\n");
+// }
+
+// void	move_right(t_game *game)
+// {
+// 	printf("Move right\n");
+// }
--- srcs/hooks.c
+++ srcs/hooks.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hocks.c                                            :+:      :+:    :+:   */
+/*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 00:15:42 by rnakatan          #+#    #+#             */
-/*   Updated: 2024/07/07 00:26:08 by rnakatan         ###   ########.fr       */
+/*   Updated: 2024/07/09 18:41:09 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "../includes/so_long.h"
 
 int		key_press(int keycode, t_game *game);
 
@@ -22,16 +22,16 @@ void	set_hooks(t_game *game)
 int	key_press(int keycode, t_game *game)
 {
 	(void)game;
-	if (keycode == 53) // ESC
-		exit(0);
-	else if (keycode == 13) // W
-		printf("Move forward\n");
-	else if (keycode == 1) // S
-		printf("Move backward\n");
-	else if (keycode == 0) // A
-		printf("Move left\n");
-	else if (keycode == 2) // D
-		printf("Move right\n");
+	if (keycode == ESC) // ESC
+		press_ESC(game);
+	// else if (keycode == 13) // W
+	// 	printf("Move forward\n");
+	// else if (keycode == 1) // S
+	// 	printf("Move backward\n");
+	// else if (keycode == 0) // A
+	// 	printf("Move left\n");
+	// else if (keycode == 2) // D
+	// 	printf("Move right\n");
 
 	printf("Key pressed: %d\n", keycode);
 	return (0);

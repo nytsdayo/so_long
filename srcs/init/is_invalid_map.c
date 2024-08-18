c4522208d554471eb3c58ec3169bb03d3f7d8aad
nytsdayo <nyantsutsu@gmail.com>
2024-08-19 08:38:31 +0900

    ノーム対策

--- srcs/init/is_invalid_map.c
+++ srcs/init/is_invalid_map.c
@@ -6,14 +6,14 @@
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:56:45 by rnakatan          #+#    #+#             */
-/*   Updated: 2024/07/20 23:26:57 by rnakatan         ###   ########.fr       */
+/*   Updated: 2024/08/19 08:33:46 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/games.h"
 
-int		is_invalid_symbol(char c);
-int		not_outwall(t_position pos, char **map);
+char	**copy_map(char **map);
+int		is_not_outwall(t_position pos, char **map);
 int		separate_check(char **map, t_player player);
 void	flood_fill(char **check_map, t_position pos);
 
@@ -29,7 +29,8 @@ int	is_invalid_map(t_game *game, char **map)
 		while (map[map_pos.y][map_pos.x])
 		{
 			c = map[map_pos.y][map_pos.x];
-			if (not_outwall(map_pos, map) || is_invalid_symbol(c))
+			if (is_not_outwall(map_pos, map) || (c != WALL && c != FLOOR
+					&& c != COLLECTIBLE && c != GOAL && c != PLAYER))
 				return (1);
 			map_pos.x++;
 		}
@@ -40,14 +41,7 @@ int	is_invalid_map(t_game *game, char **map)
 	return (0);
 }
 
-int	is_invalid_symbol(char c)
-{
-	if (c != WALL && c != FLOOR && c != COLLECTIBLE && c != GOAL && c != PLAYER)
-		return (1);
-	return (0);
-}
-
-int	not_outwall(t_position pos, char **map)
+int	is_not_outwall(t_position pos, char **map)
 {
 	if (pos.x == 0 || pos.y == 0 || !map[pos.y + 1] || !map[pos.y][pos.x + 1])
 	{
@@ -62,17 +56,10 @@ int	separate_check(char **map, t_player player)
 	char		**check_map;
 	t_position	map_pos;
 
-	map_pos = (t_position){0, 0};
 	check_map = ft_calloc(ft_cnt_row(map), sizeof(char *) * ft_cnt_row(map));
 	if (!check_map)
 		return (1);
-	while (map[map_pos.y])
-	{
-		check_map[map_pos.y] = ft_strdup(map[map_pos.y]);
-		if (!check_map[map_pos.y])
-			return (free_map(check_map), 1);
-		map_pos.y++;
-	}
+	check_map = copy_map(map);
 	flood_fill(check_map, player.point);
 	map_pos = (t_position){0, 0};
 	while (check_map[map_pos.y])
@@ -89,6 +76,25 @@ int	separate_check(char **map, t_player player)
 		map_pos.y++;
 	}
 	return (free_map(check_map), 0);
+}
+
+char	**copy_map(char **map)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	copy = ft_calloc(ft_cnt_row(map), sizeof(char *) * ft_cnt_row(map));
+	if (!copy)
+		return (NULL);
+	while (map[i])
+	{
+		copy[i] = ft_strdup(map[i]);
+		if (!copy[i])
+			return (free_map(copy), NULL);
+		i++;
+	}
+	return (copy);
 }
 
 void	flood_fill(char **map, t_position pos)
